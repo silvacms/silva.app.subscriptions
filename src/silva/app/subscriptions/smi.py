20f472170c52436392a695ffa4b4f02a8f9e4187
Sylvain Viollon <thefunny@gmail.com>
2010-09-21 11:37:14 +0000

    On the correct tab.

--- src/silva/app/subscriptions/smi.py
+++ src/silva/app/subscriptions/smi.py
@@ -4,16 +4,18 @@
 # $Id$
 
 from five import grok
-from silva.core.smi import smi as silvasmi
-from silva.translations import translate as _
-from silva.core.interfaces import ISubscribable
 from silva.app.subscriptions.interfaces import ISubscriptionService
+from silva.core.interfaces import ISubscribable
+from silva.core.smi import smi as silvasmi
+from silva.core.smi.interfaces import IPropertiesTab
+from silva.translations import translate as _
 from zope.component import queryUtility
 
 
 class SubscriptionButton(silvasmi.SMIMiddleGroundButton):
     grok.order(110)
     grok.require('silva.ManageSilvaContent')
+    grok.view(IPropertiesTab)
 
     tab = 'tab_subscriptions'
     label = _(u"subscriptions")
